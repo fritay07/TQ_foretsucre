c029fbd488144ab53c1de7216f502d6d8b212842
déborah <debobojojo@gmail.com>
2026-05-19 16:37:13 -0400

    ajout audio/musique de background pour écran titre

--- Assets/Script/AnimalClick.cs
+++ Assets/Script/AnimalClick.cs
@@ -1,25 +1,21 @@
 using UnityEngine;
 
 public class AnimalClick : MonoBehaviour
-
-public AudioClip sonDeLAnimal;
 {
-
+    public AudioClip sonDeLAnimal;
     private AudioSource audioSource;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
-        
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMouseDown()
     {
         if (sonDeLAnimal != null && !audioSource.isPlaying)
         {
             audioSource.PlayOneShot(sonDeLAnimal);
         }
-        
     }
 }
