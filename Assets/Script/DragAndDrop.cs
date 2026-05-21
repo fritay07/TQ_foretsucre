c4b31fd1bcfc36f46a459c9b8409acb52074fbba
déborah <debobojojo@gmail.com>
2026-05-20 21:10:33 -0400

    drag ad drop code

--- Assets/Script/DragAndDrop.cs
+++ Assets/Script/DragAndDrop.cs
@@ -1,13 +1,51 @@
 using UnityEngine;
-using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
-public class DragAndDrop : MonoBehaviour, IPointerDownHandler {
+public class DragAndDrop : MonoBehaviour
+{
+    private bool isDragging = false;
+    private Rigidbody2D rb;
+    private Collider2D myCollider;
+    private Camera mainCamera;
 
-    public void OnPointerDown(PointerEventData eventData)
+    void Start()
     {
-        Debug.Log("OnPointerDown");
-        // throw new System.NotImplementedException();
+        rb = GetComponent<Rigidbody2D>();
+        myCollider = GetComponent<Collider2D>();
+        mainCamera = Camera.main;
     }
 
-    
+    void Update()
+    {
+        Vector2 mouseWindowPos = Mouse.current.position.ReadValue();
+        Vector3 mouseWorldPos = mainCamera.ScreenToWorldPoint(new Vector3(mouseWindowPos.x, mouseWindowPos.y, Mathf.Abs(mainCamera.transform.position.z)));
+        Vector2 mousePos2D = new Vector2(mouseWorldPos.x, mouseWorldPos.y);
+
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            if (myCollider == Physics2D.OverlapPoint(mousePos2D))
+            {
+                isDragging = true;
+                if (rb != null)
+                {
+                    rb.gravityScale = 0;
+                    rb.linearVelocity = Vector2.zero;
+                }
+            }
+        }
+
+        if (Mouse.current.leftButton.wasReleasedThisFrame && isDragging)
+        {
+            isDragging = false;
+            if (rb != null)
+            {
+                rb.gravityScale = 1;
+            }
+        }
+
+        if (isDragging)
+        {
+            transform.position = new Vector3(mouseWorldPos.x, mouseWorldPos.y, 0f);
+        }
+    }
 }
